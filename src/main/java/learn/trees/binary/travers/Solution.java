f384c7c67016c88ac68d9c75a88e628786940581
alexcmd <alexcmd@gmail.com>
2018-03-31 22:28:38 +0300

    init

---
+++ src/main/java/learn/trees/binary/travers/Solution.java
@@ -0,0 +1,9 @@
+package learn.trees.binary.travers;
+
+import java.util.List;
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
+    }
+}
---
+++ src/main/java/learn/trees/binary/travers/TreeNode.java
@@ -0,0 +1,8 @@
+package learn.trees.binary.travers;
+
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+}
